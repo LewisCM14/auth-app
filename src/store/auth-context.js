02f6c317a1a0b6f545e1baa13e55b81d2aec613a
LewisCM14 <Lewiscmartin@live.co.uk>
2022-09-08 00:15:35 +0100

    create the auth-context provider & handler

---
+++ src/store/auth-context.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+/**
+ * context handler for the authentication state,
+ * stores the token and logged in state plus login/logout functions
+ */
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+/**
+ * context provider for authentication state,
+ * manages the token state, using it to determine user state and handle login/logout functionality
+ */
+export const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
+
+  const userIsLoggedIn = !!token; // if 'token' holds a string value user must be logged in
+
+  const loginHandler = (token) => {
+    setToken(token);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: loginHandler,
+  };
+
+  return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
